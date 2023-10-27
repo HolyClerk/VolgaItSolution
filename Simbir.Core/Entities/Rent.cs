749a0bfad13d8d9669f8e27eca3d49a8db9cbcb5
Priest <phppriest@gmail.com>
2023-10-28 01:35:35 +0500

    Rent entity

---
+++ Simbir.Core/Entities/Rent.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Simbir.Core.Entities;
+
+public class Rent
+{
+    [Key]
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(Renter))]
+    public int RenterId { get; set; }
+
+    [ForeignKey(nameof(RentTransport))]
+    public int TransportId { get; set; }
+
+    public string RentType { get; set; }
+
+    public virtual ApplicationUser Renter { get; set; }
+    public virtual Transport RentTransport { get; set; }
+}
