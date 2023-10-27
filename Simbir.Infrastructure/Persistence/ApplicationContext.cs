739a412a5e8b74c8cc505102421145b871f61480
Priest <phppriest@gmail.com>
2023-10-28 01:10:10 +0500

    Реализация интерфейса

--- Simbir.Infrastructure/Persistence/ApplicationContext.cs
+++ Simbir.Infrastructure/Persistence/ApplicationContext.cs
@@ -14,5 +14,7 @@ public class ApplicationContext : IdentityDbContext<ApplicationUser, IdentityRol
         Database.EnsureCreated();
     }
 
-    public DbSet<ApplicationUser> Users { get; }
+    public DbSet<ApplicationUser> Users { get; private set; }
+    public DbSet<Administrator> Administrators { get; private set; }
+    public DbSet<Transport> Transports { get; private set; }
 }
